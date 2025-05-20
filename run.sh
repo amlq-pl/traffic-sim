80a76c8c386632a4093f055a249203b0e7b95af0
_amlq <77460871+amlq-pl@users.noreply.github.com>
2025-05-20 22:52:08 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
-mvn javafx:run -Djavafx.args="/Users/jakubjanor/AVS/lights/src/main/resources/input.json /Users/jakubjanor/AVS/lights/src/main/resources/output.json"
+#!/bin/bash
+
+INPUT_FILE="$1"
+OUTPUT_FILE="$2"
+
+if [ -z "$INPUT_FILE" ] || [ -z "$OUTPUT_FILE" ]; then
+  echo "Usage: ./run.sh <input.json> <output.json>"
+  exit 1
+fi
+
+mvn javafx:run -Djavafx.args="$INPUT_FILE $OUTPUT_FILE"
